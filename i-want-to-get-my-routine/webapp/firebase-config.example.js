12a8675c1389c46cfa4abfeb83874eaa9128f0bd
Malafattus <berkozturk921@gmail.com>
2026-06-01 13:12:43 -0400

    Delete i-want-to-get-my-routine/webapp directory

--- i-want-to-get-my-routine/webapp/firebase-config.example.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "replace-me",
-  authDomain: "replace-me.firebaseapp.com",
-  projectId: "replace-me",
-  storageBucket: "replace-me.firebasestorage.app",
-  messagingSenderId: "replace-me",
-  appId: "replace-me",
-};
