b0536c3daab0b903702e6bea8d1d22fe7d9d44ed
Malafattus <berkozturk921@gmail.com>
2026-06-01 13:13:06 -0400

    Add files via upload

---
+++ webapp/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD0TjqxVFNtrEjQe7fY50PwSr-uy_BycBY",
+  authDomain: "routine-tracker-9c693.firebaseapp.com",
+  projectId: "routine-tracker-9c693",
+  storageBucket: "routine-tracker-9c693.firebasestorage.app",
+  messagingSenderId: "194510013417",
+  appId: "1:194510013417:web:75b544b9e348aed6148178",
+};
